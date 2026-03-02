2b717accf8499d2bb2fb6047c1e0951c8e785d6b
Mandy Lloyd <mandylloyd.tech@gmail.com>
2026-03-02 00:46:58 -0500

    integrate vitest-axe and add axe tests to Header and ProjectCard

--- src/components/Header.test.jsx
+++ src/components/Header.test.jsx
@@ -1,14 +1,20 @@
 import { render, screen } from '@testing-library/react'
 import Header from './Header';
-
+import { axe } from 'vitest-axe'
 
 it('should have aria labels on every social navigational link', () => {
-// Arrange
-render(<Header />)
+    // Arrange
+    render(<Header />)
 
-// Assert
-expect(screen.getByRole('link', { name: 'Email' })).toBeInTheDocument();
-expect(screen.getByRole('link', { name: 'GitHub' })).toBeInTheDocument();
-expect(screen.getByRole('link', { name: 'LinkedIn' })).toBeInTheDocument();
-expect(screen.getByRole('link', { name: 'Resume' })).toBeInTheDocument();
+    // Assert
+    expect(screen.getByRole('link', { name: 'Email' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'GitHub' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'LinkedIn' })).toBeInTheDocument();
+    expect(screen.getByRole('link', { name: 'Resume' })).toBeInTheDocument();
 })
+
+it('should pass axe accessibilty tests', async () => {
+    const { container } = render(<Header />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+})  
